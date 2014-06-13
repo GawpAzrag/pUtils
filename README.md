0538124243b8708b63cb3fa3858fcd8d95704cda
GawpAzrag <PJGW17@hotmail.com>
2014-06-13 09:51:09 -0700

    Added setup.py for easier distribution:  - Removed directory structure  - Rename README.md to README and updated it  - Added License header to files  - Removed Typo from License file

--- README.md
+++
@@ -1,4 +0,0 @@
-pUtils
-======
-
-Python utility library for common actions
---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='pUtils',
+      version='1.0',
+      description='Python General Utilities',
+      author='Pablo De La Garza',
+      author_email='pablo@pablodelagarza.com',
+      py_modules=['pUtils'],
+     )
